23310fecff06a1d4fcc5dac5f8bdd11383f4fe73
rautaplop <rautaplop@gmail.com>
2020-08-28 12:56:41 +0200

    Add python test visualization

--- python/test/test.py
+++ python/test/test.py
@@ -11,10 +11,23 @@
 # @author  Raul Tapia
 
 import sys
+import matplotlib.pyplot as plt
 sys.path.append("../src")
 
 import parseOptitrackCSV as optitrack
 
 if __name__ == '__main__':
     rigidBody = optitrack.RigidBody('test.csv')
-    print(rigidBody.position)
+
+    for i in sorted(rigidBody.framesWithError, reverse=True):
+        del rigidBody.time[i]
+        del rigidBody.position[i]
+
+    x = [x[0] for x in rigidBody.position]
+    y = [y[1] for y in rigidBody.position]
+    z = [x[2] for z in rigidBody.position]
+
+    plt.plot(rigidBody.time, [x[0] for x in rigidBody.position])
+    plt.plot(rigidBody.time, [y[1] for y in rigidBody.position])
+    plt.plot(rigidBody.time, [z[2] for z in rigidBody.position])
+    plt.show()
